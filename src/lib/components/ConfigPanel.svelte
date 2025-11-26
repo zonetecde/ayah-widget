2aa2d38b618a11db27e4fa2d63364c57d0c35ea4
zonetecde <zonetecde@gmail.com>
2025-11-26 08:13:29 +0100

    added reciter input

--- src/lib/components/ConfigPanel.svelte
+++ src/lib/components/ConfigPanel.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import type { Reciter } from '$lib/classes/Reciter';
+	import type { Translation } from '$lib/classes/Translation';
 	import AyahSelector from './shared/AyahSelector.svelte';
+	import ReciterSelector from './shared/ReciterSelector.svelte';
 	import TextInput from './shared/TextInput.svelte';
 	import TranslationSelector from './shared/TranslationSelector.svelte';
 
@@ -7,6 +10,7 @@
 	let surah: number = $state(2);
 	let ayah: number = $state(255);
 	let translations: Translation[] = $state([]);
+	let reciter: number | null = $state(7);
 </script>
 
 <div class="h-full w-full border-gray-400 border-2 rounded-lg p-4 min-w-[300px]">
@@ -17,4 +21,6 @@
 	<AyahSelector bind:surah bind:ayah />
 
 	<TranslationSelector bind:translations />
+
+	<ReciterSelector bind:reciter />
 </div>
